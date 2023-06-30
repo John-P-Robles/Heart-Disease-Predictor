6782d72e7e7c191bc4e2bc22cf1189a3bee8be63
John-P-Robles <111553083+John-P-Robles@users.noreply.github.com>
2023-06-29 20:18:38 -0700

    Add files via upload

---
+++ model.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+df = pd.read_csv('data_scaled.csv')
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+
+import pickle
+X = df.drop(['10-year_risk'], axis=1)
+y = df['10-year_risk']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+X_train.head()
+
+
+SVM = SVC(max_iter=1000)
+SVM.fit(X_train,y_train)
+
+pickle.dump(SVM, open('model.pkl', 'wb'))
+
+
+
+
+
+
+
