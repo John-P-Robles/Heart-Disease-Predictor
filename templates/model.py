6d62c57bd98db8087d5fea76b8e9fe219bf31128
John-P-Robles <111553083+John-P-Robles@users.noreply.github.com>
2023-09-11 15:51:57 -0700

    Update model.py

--- templates/model.py
+++ templates/model.py
@@ -1,10 +1,16 @@
-import pandas as pd
+mport pandas as pd
 
+from sklearn.model_selection import GridSearchCV
 df = pd.read_csv('data_scaled.csv')
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 
 import pickle
+param_grid = {
+    'C': [15],
+    'gamma': [2],
+    'kernel': ['rbf'],
+}
 X = df.drop(['10-year_risk'], axis=1)
 y = df['10-year_risk']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -14,5 +20,8 @@ X_train.head()
 
 SVM = SVC(max_iter=1000)
 SVM.fit(X_train,y_train)
+model = GridSearchCV(estimator=SVM, param_grid = param_grid,  param_grid, refit=True,cv=10, n_jobs=-1, verbose=3)
+model.fit(X_train,y_train)
 
 pickle.dump(SVM, open('model.pkl', 'wb'))
+pickle.dump(model, open('model.pkl', 'wb'))
