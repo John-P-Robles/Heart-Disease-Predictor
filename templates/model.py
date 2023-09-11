e9a821e60cdc227b846ea42360a3a3a89ade469b
John-P-Robles <111553083+John-P-Robles@users.noreply.github.com>
2023-09-10 23:46:07 -0700

    Update model.py

--- templates/model.py
+++ templates/model.py
@@ -2,12 +2,12 @@ import pandas as pd
 from sklearn.model_selection import GridSearchCV
 df = pd.read_csv('data_scaled.csv')
 from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 import pickle
 param_grid = {
-    'C': [15],
-    'gamma': [2],
-    'kernel': ['rbf'],
+    'max_depth': [10,20,30,40,50],
+    'n_estimators': [90,100,110,120,130],
+    'min_samples_split': [6,8,10,12,14],
 }
 X = df.drop(['10-year_risk'], axis=1)
 y = df['10-year_risk']
@@ -15,8 +15,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 X_train.head()
 
-SVM = SVC(max_iter=1000)
-model = GridSearchCV(estimator=SVM, param_grid = param_grid,  param_grid, refit=True,cv=10, n_jobs=-1, verbose=3)
+RFC = RandomForestClassifier()
+model = GridSearchCV(estimator=RFC, param_grid = param_grid,  param_grid,cv=15, n_jobs=-1, verbose=3)
 model.fit(X_train,y_train)
 
 pickle.dump(model, open('model.pkl', 'wb'))
