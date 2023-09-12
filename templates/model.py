f1d624103eed60bb2706bfd9f5c3ff12a3eb4d50
John-P-Robles <111553083+John-P-Robles@users.noreply.github.com>
2023-09-11 19:29:39 -0700

    Update model.py

--- templates/model.py
+++ templates/model.py
@@ -15,7 +15,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 X_train.head()
 
-RFC = RandomForestClassifier()
-RFC.fit(X_train,y_train)
+modl = RandomForestClassifier()
+model.fit(X_train,y_train)
 
 pickle.dump(model, open('model.pkl', 'wb'))
+
