ae39448c86922f4c63f0ae9e6c9fcea5301b253f
John-P-Robles <111553083+John-P-Robles@users.noreply.github.com>
2023-09-11 21:43:10 -0700

    Update model.py

--- model.py
+++ model.py
@@ -1,19 +1,21 @@
-from sklearn.svm import SVC
+import pandas as pd
 from sklearn.model_selection import GridSearchCV
 df = pd.read_csv('norm_CHD.csv')
-
+from sklearn.model_selection import train_test_split 
+from sklearn.ensemble import RandomForestClassifier
+import pickle
 param_grid = {
-    'C': [5,10,15],
-    'gamma': [1,2,3],
-    'kernel': ['rbf'],
-    'class_weight':['balanced', None],
+    'max_depth': [10,20,30,40,50],
+    'n_estimators': [90,100,110,120,130],
+    'min_samples_split': [6,8,10,12,14],
 }
 X = df.drop(['10-year_risk','Unnamed: 0'], axis=1)
 y = df['10-year_risk']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-SVM2 = SVC(gamma='scale')
-model = GridSearchCV(estimator=SVM2, param_grid = param_grid, refit=True,cv=10, n_jobs=-1, verbose=3)
-model.fit(X_train, y_train)
+
+
+model = RandomForestClassifier()
+model.fit(X_train,y_train)
 
 pickle.dump(model, open('model.pkl', 'wb'))
