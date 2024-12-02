0cc6281b39e0184d045916bafd104ff2e49604d9
John-P-Robles <111553083+John-P-Robles@users.noreply.github.com>
2024-12-02 14:13:12 -0800

    Update model.py

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import GridSearchCV
-df = pd.read_csv('norm_CHD.csv')
+df = pd.read_csv('data_scaled.csv')
 from sklearn.model_selection import train_test_split 
 from sklearn.ensemble import RandomForestClassifier
 import pickle
